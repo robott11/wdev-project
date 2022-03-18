dcf2780e649fe814218f461fc92c8058a2e48792
robot11 <contato.robott@gmail.com>
2022-03-17 23:18:51 -0300

    home page created

---
+++ resources/views/layouts/pages/header.blade.php
@@ -0,0 +1,8 @@
+<div class="jumbotron bg-danger p-3 my-3">
+    <h1>WDEV - MVC</h1>
+    <p>Model - View - Controller</p>
+
+    <a href=""><button class="btn btn-light">HOME</button></a>
+    <a href=""><button class="btn btn-light">SOBRE</button></a>
+    <a href=""><button class="btn btn-light">DEPOIMENTOS</button></a>
+</div>
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.pages.page')
+@section('title', 'Home')
+
+@section('content')
+    <h1>Home Page</h1>
+
+    <hr>
+
+    <h2>Robott11</h2>
+    <strong>Olá Mundo!</strong><br>
+@endsection
